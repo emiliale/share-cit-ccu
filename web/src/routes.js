bc56cd7766ae36f548326276419c5f7751a1411e
teresaacsf <56229349+teresaacsf@users.noreply.github.com>
2022-01-21 17:45:26 +0000

    Add files via upload

--- web/src/routes.js
+++ web/src/routes.js
@@ -6,8 +6,8 @@ import Main from "./pages/Main/Main";
 import Signup from "./pages/User/Signup";
 import About from "./pages/About/About";
 import Help from "./pages/Help/Help";
+import Rides from "./pages/Your Rides/yourrides";
 import Choose_rides from "./pages/Choose_rides/Choose_rides";
-
 const BaseRouter = () => (
   <div>
     <Route exact path="/" component={Main} />{" "}
@@ -15,6 +15,7 @@ const BaseRouter = () => (
     <Route exact path="/signup/" component={Signup} />{" "}
     <Route exact path="/about/" component={About} />{" "}
     <Route exact path="/help/" component={Help} />{" "}
+    <Route exact path="/your rides/" component={Rides} />{" "}
     <Route exact path="/choose_rides/" component={Choose_rides} />{" "}
   </div>
 );
